038044fa9653ec1df8613863e52ee6c281848b4f
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-12-12 20:46:58 -0800

    colors added for correct and incorrect answer. Looks horid, must fix tomorrow.

--- app.js
+++ app.js
@@ -1,84 +1,84 @@
 var questionArray = [
-		question1 = {
-			question : "What Olympics did the most UC Davis athletes compete in?",
-			correct : "London, 2012",
-			answerArray : ["London, 2012", "Los Angeles, 1984", "Pyeoncheng, 2018", "Sydney, 2000"],
-			pubImage : "<img src='images/deveres.jpg' alt='Devere's, Davis'>",
-			answerImage : "<img src='images/KingEddies.jpg' alt='King Eddies, Stratford, London'>",
-		},
+	question1 = {
+		question: "What Olympics did the most UC Davis athletes compete in?",
+		correct: "London, 2012",
+		answerArray: ["London, 2012", "Los Angeles, 1984", "Pyeoncheng, 2018", "Sydney, 2000"],
+		pubImage: "<img src='images/deveres.jpg' alt='Devere's, Davis'>",
+		answerImage: "<img src='images/KingEddies.jpg' alt='King Eddies, Stratford, London'>",
+	},
 
-		question2 = {
-			question : "What country has the most Winter Olympic medals?",
-			correct : "Norway",
-			answerArray : ["USA", "Norway", "Sweden", "Russia", "Germany", "Canada"],
-			pubImage : "<img src='images/hairyLemon.jpg' alt='Hairy Lemon Pub, Dublin, Ireland'>",
-			answerImage : "<img src='images/Oslo_Mikrobryggeri.jpg' alt='Oslo Mikrobryggeri, Oslo, Norway'>",
-		},
+	question2 = {
+		question: "What country has the most Winter Olympic medals?",
+		correct: "Norway",
+		answerArray: ["USA", "Norway", "Sweden", "Russia", "Germany", "Canada"],
+		pubImage: "<img src='images/hairyLemon.jpg' alt='Hairy Lemon Pub, Dublin, Ireland'>",
+		answerImage: "<img src='images/Oslo_Mikrobryggeri.jpg' alt='Oslo Mikrobryggeri, Oslo, Norway'>",
+	},
 
-		question3 = {
-			question : "Ireland has 16 Olympic medals in what sport?",
-			correct : "Boxing",
-			answerArray : ["Boxing", "Swiming", "Hammer Throw", "Sking", "Sailing"],
-			pubImage : "<img src='images/kytelersInn.jpg' alt='Kyteler's Inn, Kilkenny, Ireland>",
-			answerImage : "<img src='images/kateKerneys.jpg' alt='Kate Kerney's Cottage, Gap of Duloe, Ireland>",
-		},
+	question3 = {
+		question: "Ireland has 16 Olympic medals in what sport?",
+		correct: "Boxing",
+		answerArray: ["Boxing", "Swiming", "Hammer Throw", "Sking", "Sailing"],
+		pubImage: "<img src='images/kytelersInn.jpg' alt='Kyteler's Inn, Kilkenny, Ireland>",
+		answerImage: "<img src='images/kateKerneys.jpg' alt='Kate Kerney's Cottage, Gap of Duloe, Ireland>",
+	},
 
-		question4 = {
-			question : "All the Scotish athletes at the Sochi games compeated in what sport?",
-			correct : "Curling",
-			answerArray : ["Curling", "Biatholon", "Ski Cross", "Hockey", "Figure Skateing"],
-			pubImage : "<img src='images/worldsEnd.jpg' alt='The World's End, Edinburgh>",
-			answerImage : "<img src='images/sparklehorse.jpg' alt='The Sparkle Horse, Glasgow'>",
-		},
+	question4 = {
+		question: "All the Scotish athletes at the Sochi games compeated in what sport?",
+		correct: "Curling",
+		answerArray: ["Curling", "Biatholon", "Ski Cross", "Hockey", "Figure Skateing"],
+		pubImage: "<img src='images/worldsEnd.jpg' alt='The World's End, Edinburgh>",
+		answerImage: "<img src='images/sparklehorse.jpg' alt='The Sparkle Horse, Glasgow'>",
+	},
 
-		question5 = {
-			question : "What country is compeating in Bobsled for the first time this year?",
-			correct : "Nigeria",
-			answerArray : ["Nigeria", "Cuba", "South Africa", "Mexico", "Argentina", "Bolivia"],
-			pubImage : "<img src='images/mcelroy.jpg' alt='Shay McElroy's Pub, Huston, Texas>",
-			answerImage : "<img src='images/yellowChilli.jpg' alt='The Yellow Chilli, Lagos, Nigeria'>",
-		},
+	question5 = {
+		question: "What country is compeating in Bobsled for the first time this year?",
+		correct: "Nigeria",
+		answerArray: ["Nigeria", "Cuba", "South Africa", "Mexico", "Argentina", "Bolivia"],
+		pubImage: "<img src='images/mcelroy.jpg' alt='Shay McElroy's Pub, Huston, Texas>",
+		answerImage: "<img src='images/yellowChilli.jpg' alt='The Yellow Chilli, Lagos, Nigeria'>",
+	},
 
-		// question6 = {
-		// 	question : "",
-		// 	correct : "",
-		// 	answerArray : "",
-		// 	pubImage : "<img src='' alt=''>",
-		// 	answerImage : "<img src='' alt=''>",
-		// },
+	// question6 = {
+	// 	question : "",
+	// 	correct : "",
+	// 	answerArray : "",
+	// 	pubImage : "<img src='' alt=''>",
+	// 	answerImage : "<img src='' alt=''>",
+	// },
 
-		// question7 = {
-		// 	question : "",
-		// 	correct : "",
-		// 	answerArray : "",
-		// 	pubImage : "<img src='' alt=''>",
-		// 	answerImage : "<img src='' alt=''>",
-		// },
+	// question7 = {
+	// 	question : "",
+	// 	correct : "",
+	// 	answerArray : "",
+	// 	pubImage : "<img src='' alt=''>",
+	// 	answerImage : "<img src='' alt=''>",
+	// },
 
-		// question8 = {
-		// 	question : "",
-		// 	correct : "",
-		// 	answerArray : "",
-		// 	pubImage : "<img src='' alt=''>",
-		// 	answerImage : "<img src='' alt=''>",
-		// },
+	// question8 = {
+	// 	question : "",
+	// 	correct : "",
+	// 	answerArray : "",
+	// 	pubImage : "<img src='' alt=''>",
+	// 	answerImage : "<img src='' alt=''>",
+	// },
 
-		// question9 = {
-		// 	question : "",
-		// 	correct : "",
-		// 	answerArray : "",
-		// 	pubImage : "<img src='' alt=''>",
-		// 	answerImage : "<img src='' alt=''>",
-		// },
+	// question9 = {
+	// 	question : "",
+	// 	correct : "",
+	// 	answerArray : "",
+	// 	pubImage : "<img src='' alt=''>",
+	// 	answerImage : "<img src='' alt=''>",
+	// },
 
-		// question10 = {
-		// 	question : "",
-		// 	correct : "",
-		// 	answerArray : "",
-		// 	pubImage : "<img src='' alt=''>",
-		// 	answerImage : "<img src='' alt=''>",
-		// }
-	];
+	// question10 = {
+	// 	question : "",
+	// 	correct : "",
+	// 	answerArray : "",
+	// 	pubImage : "<img src='' alt=''>",
+	// 	answerImage : "<img src='' alt=''>",
+	// }
+];
 var i = 0;
 var myTimer;
 var currentQuestion;
@@ -88,24 +88,24 @@ var questionsWrong = 0;
 var questionsUnanswered = 0;
 var right = "";
 
-function shuffle (array) {
-  var a = 0;
-  var j = 0;
-  var temp = null;
+function shuffle(array) {
+	var a = 0;
+	var j = 0;
+	var temp = null;
 
-  for (a = array.length - 1; a > 0; a -= 1) {
-    j = Math.floor(Math.random() * (a + 1));
-    temp = array[a];
-    array[a] = array[j];
-    array[j] = temp;
-  }
+	for (a = array.length - 1; a > 0; a -= 1) {
+		j = Math.floor(Math.random() * (a + 1));
+		temp = array[a];
+		array[a] = array[j];
+		array[j] = temp;
+	}
 }
 
 shuffle(questionArray);
 console.log(questionArray);
 
 
-$("#startButton").on("click", function(){
+$("#startButton").on("click", function () {
 	console.log("startButton clicked");
 	$("#startButton").attr("style", "display:none;");
 	//get object
@@ -116,19 +116,21 @@ $("#startButton").on("click", function(){
 function getQuestion() {
 	console.log("getQuestion");
 
-	if (questions === questionArray.length){
+	$("#questionArea").css("background-color", "lightskyblue");
+
+	if (questions === questionArray.length) {
 		gameOver();
 	}
 	else {
-	currentQuestion = questionArray[i];
-	console.log(currentQuestion);
-	questions++;
-	printQuestion(currentQuestion);
+		currentQuestion = questionArray[i];
+		console.log(currentQuestion);
+		questions++;
+		printQuestion(currentQuestion);
 	}
 }
 
 
-//put prts in divs 
+//put parts in divs 
 function printQuestion() {
 	console.log("printQuestion");
 	$("#questionArea").append("<p>" + currentQuestion.question + "</p>");
@@ -147,18 +149,18 @@ function printQuestion() {
 
 function startTimer() {
 	var t = 30;
-	myTimer = setInterval(function(){
+	myTimer = setInterval(function () {
 		t--;
 		displayTime(t);
-		
-		if (t <= 0){
-		console.log("times up");
-		questionsUnanswered++;
-		clearInterval(myTimer);
-		var right = "time";
-		displayAnswer();
+
+		if (t <= 0) {
+			console.log("times up");
+			questionsUnanswered++;
+			clearInterval(myTimer);
+			right = "time";
+			displayAnswer();
 		}
-	},1000);
+	}, 1000);
 }
 
 function displayTime(count) {
@@ -166,7 +168,7 @@ function displayTime(count) {
 	$("#countdownArea").html(count + " seconds left!");
 }
 
-function displayAnswer () {
+function displayAnswer() {
 	i++;
 	$("#countdownArea").empty();
 	$("#answerArea").empty();
@@ -178,18 +180,18 @@ function displayAnswer () {
 	else if (right == "right") {
 		$("#questionArea").html("<p>That's right!</p>");
 	}
-	else if (right == "wrong"){
+	else if (right == "wrong") {
 		questionsWrong++;
 		$("#questionArea").html("<p>Nope. Sorry, the correct answer is " + currentQuestion.correct + "</p>");
 	}
 
-	setTimeout(function() {
+	setTimeout(function () {
 		$("#questionArea").empty();
 		getQuestion();
-		}, 3000);
+	}, 3000);
 }
 
-$(document).on("click", ".answers", function(){
+$(document).on("click", ".answers", function () {
 	console.log("answer clicked");
 
 	var answerPicked = $(this).text();
@@ -199,25 +201,27 @@ $(document).on("click", ".answers", function(){
 
 	console.log(answerPicked);
 	console.log(currentQuestion.correct);
-	
+
 	if (answerPicked.toString() == currentQuestion.correct) {
 		questionsRight++;
-		right="right";
+		right = "right";
+		$("#questionArea").css("background-color", "green");
 	}
 	else {
 		questionsWrong++;
 		right = "wrong";
+		$("#questionArea").css("background-color", "crimson");
 	}
 
 	displayAnswer();
 });
 
-function gameOver(){
+function gameOver() {
 	$("#countdownArea").empty();
 	$("answerArea").empty();
 	shuffle(questionArray);
-	i=0;
-	questions=0;
+	i = 0;
+	questions = 0;
 	$("#startButton").html("Restart?");
 	$("#startButton").attr("style", "display:block;");
 
